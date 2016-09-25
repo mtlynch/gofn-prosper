49a64c13a845eb24d3afa6eb93377b5536061193
Michael Lynch <mtlynch@google.com>
2016-09-24 20:26:47 -0400

    Splitting test common methods to their own file

---
+++ prosper/thin/test_common.go
@@ -0,0 +1,54 @@
+package thin
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"reflect"
+	"testing"
+)
+
+type values map[string]string
+
+var (
+	mux    *http.ServeMux
+	server *httptest.Server
+)
+
+func setUp() {
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+}
+
+func tearDown() {
+	server.Close()
+}
+
+func testMethod(t *testing.T, r *http.Request, want string) {
+	if got := r.Method; got != want {
+		t.Errorf("Request method: %v, want %v", got, want)
+	}
+}
+
+func testContentType(t *testing.T, r *http.Request, want string) {
+	contentType, ok := r.Header["Content-Type"]
+	if !ok || len(contentType) < 1 {
+		t.Errorf("Content-Type header not present, want: %v", want)
+		return
+	}
+	if got := r.Header["Content-Type"][0]; got != want {
+		t.Errorf("Content-type: %v, want %v", got, want)
+	}
+}
+
+func testFormValues(t *testing.T, r *http.Request, values values) {
+	want := url.Values{}
+	for k, v := range values {
+		want.Add(k, v)
+	}
+
+	r.ParseForm()
+	if got := r.Form; !reflect.DeepEqual(got, want) {
+		t.Errorf("Request parameters: %v, want %v", got, want)
+	}
+}
