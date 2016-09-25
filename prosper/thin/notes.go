2c2a1eb544bdc6d2b97325e97ac4d0448f2f48be
Michael Lynch <mtlynch@google.com>
2016-09-25 00:40:21 -0400

    Adding docs and unit test for Notes API

--- prosper/thin/notes.go
+++ prosper/thin/notes.go
@@ -3,6 +3,8 @@ package thin
 import "fmt"
 
 type (
+	// NotesResponse contains the full response from the Notes API in minimally
+	// parsed form.
 	NotesResponse struct {
 		Result      []NoteResult `json:"result"`
 		ResultCount int          `json:"result_count"`
@@ -10,6 +12,9 @@ type (
 	}
 )
 
+// Notes returns a subset of the notes that the user owns. Notes partially
+// implements the REST API described at:
+// https://developers.prosper.com/docs/investor/notes-api/
 func (c Client) Notes(offset, limit int) (response NotesResponse, err error) {
 	url := fmt.Sprintf("%s/notes/?offset=%d&limit=%d", c.baseUrl, offset, limit)
 	err = c.DoRequest("GET", url, nil, &response)
