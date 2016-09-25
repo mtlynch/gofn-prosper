6db7138cb742b91543edbe837093f6918b9dc840
Michael Lynch <mtlynch@google.com>
2016-09-25 01:09:02 -0400

    Adding docs and unit test for Accounts API (#14)

--- prosper/thin/accounts.go
+++ prosper/thin/accounts.go
@@ -1,5 +1,29 @@
 package thin
 
+// AccountsResponse contains the response from the Accounts API in minimally
+// parsed form. This struct omits a few fields. See:
+// https://github.com/mtlynch/gofn-prosper/issues/13
+type AccountsResponse struct {
+	AvailableCashBalance                float64 `json:"available_cash_balance"`
+	PendingInvestmentsPrimaryMarket     float64 `json:"pending_investments_primary_market"`
+	PendingInvestmentsSecondaryMarket   float64 `json:"pending_investments_secondary_market"`
+	PendingQuickInvestOrders            float64 `json:"pending_quick_invest_orders"`
+	TotalPrincipalReceivedOnActiveNotes float64 `json:"total_principal_received_on_active_notes"`
+	TotalAmountInvestedOnActiveNotes    float64 `json:"total_amount_invested_on_active_notes"`
+	OutstandingPrincipalOnActiveNotes   float64 `json:"outstanding_principal_on_active_notes"`
+	TotalAccountValue                   float64 `json:"total_account_value"`
+	InflightGross                       float64 `json:"inflight_gross"`
+	LastDepositAmount                   float64 `json:"last_deposit_amount"`
+	LastDepositDate                     string  `json:"last_deposit_date"`
+	LastWithdrawAmount                  float64 `json:"last_withdraw_amount"`
+	LastWithdrawDate                    string  `json:"last_withdraw_date"`
+	ExternalUserId                      string  `json:"external_user_id"`
+}
+
+// Accounts queries the Propser API for properties of the user's account,
+// including balance information and note summaries. Accounts partially
+// implements the REST API described at:
+// https://developers.prosper.com/docs/investor/accounts-api/
 func (c Client) Accounts() (response AccountsResponse, err error) {
 	err = c.DoRequest("GET", c.baseUrl+"/accounts/prosper/", nil, &response)
 	if err != nil {
