30c8e7a2e87ef020bcb883ca1552a7d6c16ed8ec
Michael Lynch <mtlynch@google.com>
2016-09-28 20:11:23 -0400

    Adding functions to generate new clients (#23)

--- prosper/client.go
+++ prosper/client.go
@@ -1,7 +1,9 @@
 package prosper
 
 import (
+	"github.com/mtlynch/gofn-prosper/auth"
 	"github.com/mtlynch/gofn-prosper/prosper/thin"
+	"github.com/mtlynch/gofn-prosper/types"
 )
 
 type Client struct {
@@ -10,4 +12,15 @@ type Client struct {
 	nrp           notesResponseParser
 	listingParser listingParser
 	orderParser   orderParser
+}
+
+func NewClient(creds types.ClientCredentials) Client {
+	tokenMgr := auth.NewTokenManager(auth.NewAuthenticator(creds))
+	return Client{
+		rawClient:     thin.NewClient(tokenMgr),
+		ap:            defaultAccountsParser{},
+		nrp:           NewNotesResponseParser(),
+		listingParser: defaultListingParser{},
+		orderParser:   defaultOrderParser{},
+	}
 }
